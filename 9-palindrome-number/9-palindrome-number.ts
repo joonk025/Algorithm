64e634d6f8e070aaabf85461aa00f0fe8ac417d4
Joon Kim <61416890+joonk025@users.noreply.github.com>
2022-01-14 20:49:54 +0900

    Time: 346 ms (21.12%), Space: 49.2 MB (19.82%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.ts
+++ 9-palindrome-number/9-palindrome-number.ts
@@ -1,16 +1,10 @@
 function isPalindrome(x: number): boolean {
-    // convert x to a string
     const xString = x.toString();
-    
-    // split the string into an array
     const xArray = xString.split('');
     
-    // compare the first and last number using recursion
     const compareFirstAndLast = (arr: string[] = xArray): boolean => {
-        // base case: if the length of array is less or equal to 1, return true
         if (arr.length <= 1) { return true }
         
-        // if first and last elements are the same, remove them and recurse
         if (arr[0] === arr[arr.length - 1]) {
             const newArr = arr.slice(1,-1);
             return compareFirstAndLast(newArr)
@@ -18,6 +12,5 @@ function isPalindrome(x: number): boolean {
             return false;
         }
     }
-    
     return compareFirstAndLast(xArray);
 };
