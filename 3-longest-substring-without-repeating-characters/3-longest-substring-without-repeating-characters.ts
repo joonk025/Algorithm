0491a270f86e8184a5cd065af3e2fa33a8529d8e
Joon Kim <61416890+joonk025@users.noreply.github.com>
2022-01-17 13:22:37 +0900

    Time: 214 ms (39.48%), Space: 45.7 MB (35.27%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.ts
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.ts
@@ -3,21 +3,12 @@ function lengthOfLongestSubstring(s: string): number {
     let maxLength = 0;
 
     for (let i = 0; i < s.length; i += 1) {
-//         if (longestStr.split('').includes(s[i])) {
-//             longestStr = longestStr.slice(longestStr.split('').indexOf(s[i]) + 1);
-//         }
-//          longestStr += s[i];
-        
-//         if (longestStr.length > maxLength) {maxLength = longestStr.length} 
- 
-        longestStr += s[i];
-        if(longestStr.indexOf(s[i]) !== longestStr.length - 1) {
-            longestStr = longestStr.slice(longestStr.indexOf(s[i]) + 1)
+        if (longestStr.split('').includes(s[i])) {
+            longestStr = longestStr.slice(longestStr.split('').indexOf(s[i]) + 1);
         }
+         longestStr += s[i];
         
         if (longestStr.length > maxLength) {maxLength = longestStr.length} 
-        console.log(maxLength, longestStr)
-        
     }
     return maxLength;
 };
