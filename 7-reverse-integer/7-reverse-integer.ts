5c953efb8b45fcd169fa5713379e3b0ac96cb69e
Joon Kim <61416890+joonk025@users.noreply.github.com>
2022-01-17 18:19:52 +0900

    Time: 133 ms (31.96%), Space: 39.9 MB (96.24%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.ts
+++ 7-reverse-integer/7-reverse-integer.ts
@@ -4,7 +4,6 @@ function reverse(x: number): number {
         sum = sum * 10 + x % 10
         x = Math.trunc(x / 10);
     }
-
     return sum < -2147483648 || sum > 2147483647 ? 0 : sum;
 };
     
