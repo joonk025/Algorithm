e61bda0713fade3aae941b004a1f2ea58ebd8696
Joon Kim <61416890+joonk025@users.noreply.github.com>
2022-02-02 17:07:47 +0900

    Time: 72 ms (92.91%), Space: 43.8 MB (5.51%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.ts
+++ 58-length-of-last-word/58-length-of-last-word.ts
@@ -1,20 +1,3 @@
-// function lengthOfLastWord(s: string): number {
-//     let maxL = 0;
-    
-//     if(s === " ") return 0;
-    
-//     for (let i = s.length - 1; i >= 0; i -= 1) { 
-//         if (s[i] !== ' ') {
-//             maxL += 1;
-//         } else if (s[i] === ' ' && maxL !== 0) {
-//             return maxL;
-//         }
-//     }
-//     return maxL;
-// };
-
-
 function lengthOfLastWord(s: string): number {
-    const newS = s.trim().replace(/[^a-z]/gi, '@');
-    return newS.length - newS.lastIndexOf('@') - 1;
+    return s.trim().split(' ').pop().length; 
 }
