8af4bac9a647ec2ead0538b94f881921d9645c3c
Joon Kim <61416890+joonk025@users.noreply.github.com>
2022-03-15 13:45:23 -0700

    Time: 91 ms (61.21%), Space: 43.7 MB (49.26%) - LeetHub

--- 27-remove-element/27-remove-element.ts
+++ 27-remove-element/27-remove-element.ts
@@ -1,16 +1,10 @@
 function removeElement(nums: number[], val: number): number {
-    let numberOfDeletes = 0
-
-    for (let i = 0; i < nums.length; i++) {
-        const number = nums[i]
-        
-        if (number === val) {
-            numberOfDeletes += 1
-            nums[i] = Infinity
-        }
-    }
+ for (let i = 0; i < nums.length; i += 1) {
+     if (nums[i] === val) {
+         nums.splice(i, 1);
+         i -= 1;
+     }
+ }
     
-    nums.sort((a, b) => a - b)
-    
-    return nums.length - numberOfDeletes
+    return nums.length;
 };
