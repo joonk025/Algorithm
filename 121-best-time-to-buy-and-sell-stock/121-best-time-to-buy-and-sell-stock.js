a3a7b0e63c87576b272be4e57cff5c37de52248e
Joon Kim <61416890+joonk025@users.noreply.github.com>
2022-03-18 17:32:14 -0700

    Time: 88 ms (84.39%), Space: 52.2 MB (12.92%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -5,17 +5,14 @@
 var maxProfit = function(prices) {
     let min = prices[0];
     let maxDiff = 0;
-    
     for (let i = 0; i < prices.length; i += 1) {
         if (prices[i] < min) {
             min = prices[i];
             continue;
         }
-        
         if (prices[i] - min > maxDiff) {
             maxDiff = prices[i] - min;
         }
     }
-    
     return maxDiff;
 };
