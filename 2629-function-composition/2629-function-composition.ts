e364a9926be96234e2b69cd64ca85ee0542641fb
Joon Kim <61416890+joonk025@users.noreply.github.com>
2023-07-02 12:39:02 -0700

    Time: 66 ms (94.09%), Space: 46.1 MB (30.15%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.ts
@@ -0,0 +1,22 @@
+type F = (x: number) => number;
+// input is an array of functions
+// output is a number
+
+
+function compose(functions: F[]): F {
+    functions = functions.reverse();
+    let index = 0;
+    
+	return function calculate (x: number): number {
+        if (index === functions.length) return x;
+        
+        const func = functions[index];
+        index += 1;
+        return calculate(func(x));
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
