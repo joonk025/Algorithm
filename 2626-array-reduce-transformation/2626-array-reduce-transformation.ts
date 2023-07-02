7859cef600000d4ad3e5da01c737a4f362350ac8
Joon Kim <61416890+joonk025@users.noreply.github.com>
2023-07-02 12:05:13 -0700

    Time: 73 ms (22.08%), Space: 44.8 MB (59.32%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.ts
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.ts
@@ -1,15 +1,11 @@
 type Fn = (accum: number, curr: number) => number
 
 function reduce(nums: number[], fn: Fn, init: number): number {
-    if (nums === []) return init;
-
     let acc: number = init;
-    let index: number = 0;
     
-    while (index < nums.length) {
-        acc = fn(acc, nums[index]);
-        index += 1;
+    for (let i = 0; i < nums.length; i += 1) {
+        acc = fn(acc, nums[i]);
     }
-    return acc;
 
+    return acc;
 };
