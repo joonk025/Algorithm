34a055fc5814a3248142856f1f3bdca92eca4f48
Joon Kim <61416890+joonk025@users.noreply.github.com>
2023-10-19 23:48:18 -0700

    Time: 58 ms (47.77%), Space: 44.9 MB (17.59%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.ts
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.ts
@@ -1,33 +1,47 @@
-function mergeAlternately(word1: string, word2: string): string {
-// declare a variable that stores a result as an empty string
-    let mergedString: string = "";
-    
-// figure which word is longer
-    let longerWord: string;
-    let shorterWord: string;
+function mergeAlternately (word1: string, word2: string): string {
 
-    if (word1.length >= word2.length) {
-        longerWord = word1;
-        shorterWord = word2;
-    } else {
-        longerWord = word2;
-        shorterWord = word1;
+    let mergedString: string = '';
+
+    for (let i = 0; i < Math.max(word1.length, word2.length); i += 1) {
+        if (i < word1.length) mergedString += word1[i];
+        if (i < word2.length) mergedString += word2[i];
     }
+    return mergedString;
+}
+
+
+
+
+// function mergeAlternately(word1: string, word2: string): string {
+// // declare a variable that stores a result as an empty string
+//     let mergedString: string = "";
     
-// iterate through the longer word
-    for (let i = 0; i < longerWord.length; i += 1) {
-     // if the character corresponding to the index for longerWord exists, append it to result
-        if (word1[i]) {
-            mergedString += word1[i];
-        }
-    // if the character corresponding to the index for shorterWord exists, append it to result
-        if (word2[i]) {
-            mergedString += word2[i];
-        }
-    }
+// // figure which word is longer
+//     let longerWord: string;
+//     let shorterWord: string;
+
+//     if (word1.length >= word2.length) {
+//         longerWord = word1;
+//         shorterWord = word2;
+//     } else {
+//         longerWord = word2;
+//         shorterWord = word1;
+//     }
+    
+// // iterate through the longer word
+//     for (let i = 0; i < longerWord.length; i += 1) {
+//      // if the character corresponding to the index for longerWord exists, append it to result
+//         if (word1[i]) {
+//             mergedString += word1[i];
+//         }
+//     // if the character corresponding to the index for shorterWord exists, append it to result
+//         if (word2[i]) {
+//             mergedString += word2[i];
+//         }
+//     }
    
     
     
-// return mergedString
-    return mergedString;
-};
+// // return mergedString
+//     return mergedString;
+// };
